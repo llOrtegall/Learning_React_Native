8932050d87ba8ced76f7a010f31d7f1feb7b520c
llOrtegall <IvanOrtega_97@hotmail.com>
2024-10-04 09:11:49 -0500

    works select image funtion

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,13 @@ import { View, StyleSheet,  } from 'react-native';
 import ImageViewer from '@/components/ImageViewer';
 import Button from '@/components/Button';
 import * as ImagePiker from 'expo-image-picker';
+import { useState } from 'react';
 
 const Placeholder = require('../../assets/background-image.png');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
   const pickImageAsync = async () => {
     let result = await ImagePiker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -14,6 +17,7 @@ export default function Index() {
 
     if(!result.canceled){
       console.log(result);
+      setSelectedImage(result.assets[0].uri);
     } else {
       alert('You must pick an image to continue.');
     }
@@ -22,10 +26,10 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={Placeholder} />
+        <ImageViewer imgSource={Placeholder} selectedImage={selectedImage}/>
       </View>
       <View style={styles.footerContainer}>
-        <Button label='Choose a photo' theme='primary' />
+        <Button label='Choose a photo' theme='primary' onPress={pickImageAsync}/>
         <Button label='Take a photo' />
       </View>
     </View>
