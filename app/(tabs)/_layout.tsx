0d00df8074c763056e8af7365f93f92b40f66e4b
llOrtegall <IvanOrtega_97@hotmail.com>
2024-10-04 09:05:23 -0500

    Refactor dependencies and add image picker functionality

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,22 +3,23 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs 
-      screenOptions={{
-        tabBarActiveTintColor: '#007aff',
-      }}>
-      <Tabs.Screen name="index" options={{ 
+    <Tabs screenOptions={{ tabBarActiveTintColor: '#007aff', }} >
+      <Tabs.Screen name="index" options={{
         title: 'Home',
+        headerTitleStyle: { color: '#fff' },
+        headerStyle: { backgroundColor: '#25292e' },
         tabBarIcon: ({ color, focused }) => (
           <Ionicons name={focused ? 'home-sharp' : 'home-outline'} size={24} color={color} />
-        ) 
-      }}  />
-      <Tabs.Screen name="about" options={{ 
-        title: 'About', 
-        tabBarIcon: ({ color, focused }) => (
-          <Ionicons name={ focused ? 'information-circle' : 'information-circle-outline' } size={24} color={color} />
         )
-        }} />
+      }} />
+      <Tabs.Screen name="about" options={{
+        title: 'Home',
+        headerTitleStyle: { color: '#fff' },
+        headerStyle: { backgroundColor: '#25292e' },
+        tabBarIcon: ({ color, focused }) => (
+          <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} size={24} color={color} />
+        )
+      }} />
     </Tabs>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,33 @@
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet,  } from 'react-native';
+import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
+import * as ImagePiker from 'expo-image-picker';
 
 const Placeholder = require('../../assets/background-image.png');
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePiker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    })
+
+    if(!result.canceled){
+      console.log(result);
+    } else {
+      alert('You must pick an image to continue.');
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Image source={Placeholder} style={styles.image} />
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={Placeholder} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label='Choose a photo' theme='primary' />
+        <Button label='Take a photo' />
+      </View>
     </View>
   );
 }
@@ -14,14 +36,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-     alignItems: 'center',
+    alignItems: 'center',
   },
   imageContainer: {
-    flex: 1
+    flex: 1,
+    paddingTop: 28
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
   }
 });
